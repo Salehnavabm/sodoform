ac873ef806e75cf5a5393f21e41626645de16857
salehnavab <salehnavabm@gmail.com>
2023-07-04 12:32:22 +0430

    Add project files.

---
+++ sodoform/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace sodoform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sudokuTextBox = new System.Windows.Forms.TextBox();
+            this.solutionTextBox = new System.Windows.Forms.TextBox();
+            this.SolveSudok = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sudokuTextBox
+            // 
+            this.sudokuTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.sudokuTextBox.Location = new System.Drawing.Point(12, 46);
+            this.sudokuTextBox.Multiline = true;
+            this.sudokuTextBox.Name = "sudokuTextBox";
+            this.sudokuTextBox.Size = new System.Drawing.Size(200, 199);
+            this.sudokuTextBox.TabIndex = 1;
+            this.sudokuTextBox.TextChanged += new System.EventHandler(this.sudokuTextBox_TextChanged);
+            // 
+            // solutionTextBox
+            // 
+            this.solutionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.solutionTextBox.Location = new System.Drawing.Point(294, 46);
+            this.solutionTextBox.Multiline = true;
+            this.solutionTextBox.Name = "solutionTextBox";
+            this.solutionTextBox.Size = new System.Drawing.Size(185, 199);
+            this.solutionTextBox.TabIndex = 2;
+            this.solutionTextBox.TextChanged += new System.EventHandler(this.solutionTextBox_TextChanged);
+            // 
+            // SolveSudok
+            // 
+            this.SolveSudok.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.SolveSudok.Location = new System.Drawing.Point(16, 251);
+            this.SolveSudok.Name = "SolveSudok";
+            this.SolveSudok.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.SolveSudok.Size = new System.Drawing.Size(91, 31);
+            this.SolveSudok.TabIndex = 3;
+            this.SolveSudok.Text = "حل سودوکو";
+            this.SolveSudok.UseVisualStyleBackColor = true;
+            this.SolveSudok.Click += new System.EventHandler(this.SolveSudok_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(8, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "سودوکو مورد نظر خود را وارد کنید ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(309, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "حل سودوکو وارد شده";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.Location = new System.Drawing.Point(394, 251);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(91, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "خروج";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button2.Location = new System.Drawing.Point(204, 251);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button2.Size = new System.Drawing.Size(91, 31);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "راهنما";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 286);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SolveSudok);
+            this.Controls.Add(this.solutionTextBox);
+            this.Controls.Add(this.sudokuTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sudokuTextBox;
+        private System.Windows.Forms.TextBox solutionTextBox;
+        private System.Windows.Forms.Button SolveSudok;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ sodoform/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sodoform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }    
+
+        private void sudokuTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void solutionTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }   
+
+        private void SolveSudok_Click(object sender, EventArgs e)
+        {
+            string sodok = sudokuTextBox.Text;
+
+            // پارس کردن سودوکو و ذخیره در ماتریس
+            int[,] sudokuBoard = new int[9, 9];
+            //اگه فاصله خالی یا خطوط مشکل داشتن درستشون میکنه 
+            string[] rows = sodok.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (rows.Length != 9)
+            {
+                //اگه تعداد سطر بیشتر یا کمتر 9 تا باشند این پیام چاپ میشه
+                MessageBox.Show(" !!!ورودی سودوکو نا معتبر است ");
+                return;
+
+            }
+            // اعداد را با کاراکتر فاصله هم چک میکنه واگه اعداد مساوی 9 تا نبودند ارور میده
+            for (int i = 0; i < 9; i++)
+            {
+                string[] numbers = rows[i].Split(' ');
+                if (numbers.Length != 9)
+                {
+                    MessageBox.Show(" !!!ورودی سودوکو نا معتبر است ");
+                    return;
+                }
+
+                for (int j = 0; j < 9; j++)
+                {
+                    if (!int.TryParse(numbers[j], out int value))
+                    {
+                        MessageBox.Show(" !!!ورودی سودوکو نا معتبر است ");
+                        return;
+                    }
+
+                    sudokuBoard[i, j] = value;
+                }
+            }
+
+            // حل سودوکو
+            if (SolveSudoku(sudokuBoard))
+            {
+                string solution = "";
+
+                // تبدیل ماتریس حل شده به رشته
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        solution += sudokuBoard[i, j] + " ";
+                    }
+
+                    solution += Environment.NewLine;
+                }
+
+                solutionTextBox.Text = solution;
+            }
+            else
+            {
+                MessageBox.Show("!!!هیچ راه حلی برای حل این سودوکو وجود ندارد");
+            }
+        }
+
+        private bool SolveSudoku(int[,] board)
+        {
+            int row = 0;
+            int col = 0;
+            if (!find(board, ref row, ref col))
+            {
+                return true; // تمامی خانه‌ها پر شده و سودوکو حل شده است
+            }
+
+            // امتحان کردن اعداد از ۱ تا ۹ در هر خانه خالی
+            for (int num = 1; num <= 9; num++)
+            {
+                if (isSafe(board, row, col, num))
+                {
+                    board[row, col] = num;
+
+                    if (SolveSudoku(board))
+                    {
+                        return true;
+                    }
+
+                    board[row, col] = 0; // اگر حل درست نبود، آن را بازگشت داده و خانه را خالی می‌کنیم
+                }
+            }
+
+            return false; // هیچ عدد مناسبی پیدا نشد
+        }
+
+        private bool find(int[,] board, ref int row, ref int col)
+        {
+            for (row = 0; row < 9; row++)
+            {
+                for (col = 0; col < 9; col++)
+                {
+                    if (board[row, col] == 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool isSafe(int[,] board, int str, int sot, int num)
+        {
+            // بررسی آیا عدد در این ردیف تکراری است
+            for (int d = 0; d < 9; d++)
+            {
+                if (board[str, d] == num)
+                {
+                    return false;
+                }
+            }
+            for (int r = 0; r < 9; r++)
+            {
+                if (board[r, sot] == num)
+                {
+                    return false;
+                }
+            }
+
+            // بررسی آیا عدد در یک بلوک ۳x۳ تکراری است
+            int startRow = str - (str % 3);
+            int startCol = sot - (sot % 3);
+            for (int r = 0; r < 3; r++)
+            {
+                for (int d = 0; d < 3; d++)
+                {
+                    if (board[r + startRow, d + startCol] == num)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true; // عدد مناسبی پیدا شده است
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("آیا می خواهید خارج شوید؟", "اخطار", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("سودوکو خودرا به فرمت زیر وارد کنید \n0 0 3 0 2 0 6 0 0\n9 0 0 3 0 5 0 0 1\n0 0 1 8 0 6 4 0 0\n0 0 8 1 0 2 9 0 0\n7 0 0 0 0 0 0 0 8\n0 0 6 7 0 8 2 0 0\n0 0 2 6 0 9 5 0 0\n8 0 0 2 0 3 0 0 9\n0 0 5 0 1 0 3 0 0");
+
+        }
+        
+    }
+    
+}
